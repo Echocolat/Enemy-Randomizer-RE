8e1dbaf598c488cf56f7cd8c0e2fbbfd56afd44d
Echocolat <98425838+Echocolat@users.noreply.github.com>
2022-08-18 12:06:24 +0200

    added divine beasts

--- main.py
+++ main.py
@@ -63,3 +63,17 @@ def randomize_all_dlc_shrines(chaosmode = RANDO_CONFIG['chaosMode']):
 
         with open('Enemizer\\aoc\\0010\\'+file,'wb') as f:
             f.write(file_data)
+
+def randomize_divine_beasts(chaosmode = RANDO_CONFIG['chaosMode']):
+    for file in FILE_LIST['Divine Beast packs']:
+
+        file_data = util.get_game_file(file, aoc=True).read_bytes()
+        file_data = randomize_pack(file_data,chaosmode,file,aoc=True)
+
+        folder = os.path.join('Enemizer\\aoc\\0010\\Pack')
+        os.makedirs(folder, exist_ok = True)
+
+        with open('Enemizer\\aoc\\0010\\'+file,'wb') as f:
+            f.write(file_data)
+
+randomize_divine_beasts()
